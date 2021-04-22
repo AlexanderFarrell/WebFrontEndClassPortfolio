69b31e0181dcadfdd8b3fa184c2c3c63804ff800
MorphSight <irishsnowday@icloud.com>
2021-04-21 19:28:39 -0700

    Main Page (index.html) now showing links to table of contents. JavaScript added to automatically populate this, and a simple stylesheet added.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let tableOfContents = document.getElementById('TableOfContents');
+
+const links = [
+    {
+        label: "Week1 Notes",
+        url: "week1/index.html"
+    }
+]
+
+links.forEach(link => {
+    let item = document.createElement("li");
+    item.innerHTML = `<a class="TableOfContentsItem" href="${link.url}">${link.label}</a>`;
+    tableOfContents.appendChild(item);
+});
